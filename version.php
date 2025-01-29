acdc28c3e67c83585c27af58fa943cf8415743bb
Ponlawat W. <ponlawat_w@outlook.co.th>
2025-01-29 16:29:06 +0700

    Bumped version.php

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'accessibility_fontkerning';
-$plugin->release = '1.0.0';
-$plugin->version = 2022041900;
+$plugin->release = '1.0.1';
+$plugin->version = 2025012900;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
